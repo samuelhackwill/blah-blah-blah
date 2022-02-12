ccbd12eafbf62f82af6e32d885271d6337809097
Samuel Hackwill <hackmew@gmail.com>
2022-02-12 16:30:06 +0100

    Add CSS struff to position label & keyboard events on name input

--- imports/ui/pages/edit/edit.js
+++ imports/ui/pages/edit/edit.js
@@ -38,10 +38,44 @@ Template.edit.helpers({
 	discussionLinesData(){
 		return{
 			discussionLinesData:DiscussionLines.find({}).fetch(),
-			_talkerName : this.talkerName.get(), 
+			_talkerName : Template.instance().talkerName.get(), 
 			_listenerName : Template.instance().listenerName.get(),
 			_talkerColor : Template.instance().talkerColor.get(), 
 			_listenerColor : Template.instance().listenerColor.get()
 		}
+	}
+})
+
+
+Template.edit.events({
+	"click .colorPicker":function(e){
+
+		// the system here makes it possible that both peeps
+		// will have the same color. We could change that
+		// by splicing the array rather than reading it
+		// and replacing the color the end of the array
+		// everytime we change color.
+
+		if (e.target.id == "colorPicker1") {
+			_target = "talkerColor"
+		}else{
+			_target = "listenerColor"
+		}
+
+		randomNmbr = Math.floor(Math.random()*allCssNamedColors.length)
+		Template.instance()[_target].set(allCssNamedColors[randomNmbr])
+
+	},
+	
+	"keyup .nameForm":function(e){
+
+		if (e.target.name == "name1") {
+			_target = "talkerName"
+		}else{
+			_target = "listenerName"
+		}
+
+		Template.instance()[_target].set(e.target.value)
+
 	}
 })
--- imports/ui/pages/new/new.js
+++ imports/ui/pages/new/new.js
@@ -7,8 +7,6 @@ import '../../components/conversationParams.js';
 import '../../components/conversationEditor.js';
 import '../../components/conversationSaver.js';
 
-const allCssNamedColors = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","DarkOrange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","Lime","LimeGreen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","RebeccaPurple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"]
-
 
 Template.new.onCreated(function(){
 	// console.log(this)
@@ -68,6 +66,18 @@ Template.new.events({
 
 		randomNmbr = Math.floor(Math.random()*allCssNamedColors.length)
 		Template.instance()[_target].set(allCssNamedColors[randomNmbr])
+
+	},
+
+	"keyup .nameForm":function(e){
+
+		if (e.target.name == "name1") {
+			_target = "talkerName"
+		}else{
+			_target = "listenerName"
+		}
+
+		Template.instance()[_target].set(e.target.value)
 
 	}
 })
