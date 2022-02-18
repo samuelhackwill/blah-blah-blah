34dc01aae15f26110d0fa8621792aa1f49d5f092
Samuel Hackwill <hackmew@gmail.com>
2022-02-18 16:08:58 +0100

    Add working text wrapping for the getline function. Will still break in case of very long string

--- imports/ui/components/theDrawing.js
+++ imports/ui/components/theDrawing.js
@@ -28,9 +28,15 @@ Template.theDrawing.helpers({
 	},
 	
 	getLine() {
+
+    // this function's goal is to retrieve text and to split it in
+    // one to six lines. One very long word breaks the wrapping.
+
 		index = Template.instance().counter.get();
 		getText = Template.instance().data.discussion.discussionLines[index].lineContent
-    counter = 0
+    wrapCurrentWord = false
+    wrapCounter = 0
+    lineCounter = 1
     lines = {line1:[], line2:[], line3:[], line4:[], line5:[], line6:[]}
     longletters = ["w","m"]
     // max : 23 ... so 4,3 ratio
@@ -40,35 +46,60 @@ Template.theDrawing.helpers({
     // letters and numbers should default to medium
     // non-word (spaces, punctuation, etc) should default to small.
 
-    for (var i = 0 ; i < getText.length; i++) {
-      if(counter<100) lines.line1.push(getText[i])
-      if(counter>=100 && counter<200) lines.line2.push(getText[i])
-      if(counter>=200 && counter<300) lines.line3.push(getText[i])
-      if(counter>=300 && counter<400) lines.line4.push(getText[i])
-      if(counter>=400 && counter<500) lines.line5.push(getText[i])
-      if(counter>=500 && counter<600) lines.line6.push(getText[i])
+    words = getText.split(" ")
 
-      if (getText[i] == "w" || getText[i] == "m"){
-        counter = counter + 2.8   
+    for (var z = 0 ; z < words.length; z++) {
+      for (var i = 0 ; i < words[z].length; i++){
+        // FOR EVERY LETTER
+        // increment counter, that's it
+        if (words[z][i]== "w" || words[z][i]== "m"){
+          wrapCounter = wrapCounter + 2.8   
+        }
+
+        if (words[z][i]== "i" || words[z][i]== "j" || words[z][i]== "l" || words[z][i]== "f" || words[z][i]== "r" || words[z][i] == "t"){
+          wrapCounter = wrapCounter - 1.3   
+        }
+
+        wrapCounter = wrapCounter + 2.8
+
+        console.log(wrapCounter, words[z])
+
+        // we want to check, at EVERY letter, if we need to wrap current word,
+        // and reset counter AS SOON as a letter is over 100.
+        if (wrapCounter>=100) {
+          console.log(wrapCounter, words[z])
+          console.log("wrap at ", words[z])
+          wrapCounter = 0
+          wrapCurrentWord = true
+        }
+
+      }
+      // also increment counter for spaces!
+      wrapCounter = wrapCounter + 2.8
+
+      // FOR EVERY WORD
+      // check wrapCounter
+      // insert "wrap" if wrapCounter > x
+      // reset wrapCounter
+
+      if (wrapCurrentWord) {
+        lineCounter = lineCounter + 1
+        wrapCurrentWord = false
       }
 
-      if (getText[i] == "i" || getText[i] == "j" || getText[i] == "l" || getText[i] == "f" || getText[i] == "r" || getText[i] == "t"){
-        counter = counter - 1.3   
-      }
-
-      counter = counter + 2.8
+      selector = "line"+lineCounter
+      // console.log(selector, lines[selector], words[z])
+      lines[selector].push(words[z])
 
     }
 
-    console.log(lines.line1.join(''))
-
     return {
-      line1:lines.line1.join(''),
-      line2:lines.line2.join(''),
-      line3:lines.line3.join(''),
-      line4:lines.line4.join(''),
-      line5:lines.line5.join(''),
-      line6:lines.line6.join('')
+      line1:lines.line1.join(' '),
+      line2:lines.line2.join(' '),
+      line3:lines.line3.join(' '),
+      line4:lines.line4.join(' '),
+      line5:lines.line5.join(' '),
+      line6:lines.line6.join(' ')
     }
   },
 
